ad9deca9d0736e7d433c77dab2e753ac4fb9b1e3
Arnaud Cordier <159036689+Cordierarn@users.noreply.github.com>
2026-04-09 14:54:25 +0200

    Rename class and update variable names in script

--- scripts/tags_per_movie.py
+++ scripts/tags_per_movie.py
@@ -3,24 +3,21 @@ Q1 - Combien de tags chaque film possede-t-il ?
 Configuration Hadoop par defaut.
 """
 from mrjob.job import MRJob
-from mrjob.step import MRStep
 
+class TagsParFilm(MRJob):
 
-class TagsPerMovie(MRJob):
-
-    def mapper(self, _, line):
+    def mapper(self, _, ligne):
         try:
-            parts = line.split(',')
-            if parts[0] == 'userId':
+            elements = ligne.split(',')
+            if elements[0] == 'userId':
                 return  # ignorer l'en-tete
-            movieId = parts[1]
-            yield movieId, 1
+            id_film = elements[1]
+            yield id_film, 1
         except Exception:
             pass
 
-    def reducer(self, movieId, counts):
-        yield movieId, sum(counts)
-
+    def reducer(self, id_film, occurences):
+        yield id_film, sum(occurences)
 
 if __name__ == '__main__':
-    TagsPerMovie.run()
+    TagsParFilm.run()
